48cca34cfcca70b690644ac1e82332ebd8f5c73f
Merve Gulenay perseke <mgulenay@c2s4d11.42wolfsburg.de>
2022-11-08 22:08:40 +0100

    ex00

--- CPP04/ex00/Cat.cpp
+++ CPP04/ex00/Cat.cpp
@@ -6,17 +6,41 @@
 /*   By: mgulenay <mgulenay@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:15:27 by mgulenay          #+#    #+#             */
-/*   Updated: 2022/11/08 16:17:09 by mgulenay         ###   ########.fr       */
+/*   Updated: 2022/11/08 21:26:14 by mgulenay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 
-Cat::Cat(/* args */)
+Cat::Cat(/* args */): Animal("Cat")
 {
+	std::cout << GREEN << this->_type << "Default Cat constructoR called " << RESET << std::endl;
+}
+
+Cat::Cat(const Cat &source_obj)
+{
+	std::cout << YELLOW << "Cat Copy ConstructoR called" << RESET << std::endl;
+
+	*this = source_obj;
+}
+
+Cat &Cat::operator=(const Cat &source_obj)
+{
+	std::cout << BLUE << "Cat Assignment OperatoR called" << RESET << std::endl;
+
+	if (this != &source_obj)
+	{
+		this->_type = source_obj._type;
+	}
+	return (*this);
+}
+
+STR Cat::getType() const
+{
+	return (this->_type);
 }
 
 Cat::~Cat()
 {
+	std::cout << GREEN << "Cat destructoR called " << RESET << std::endl;
 }
-
--- CPP04/ex00/Dog.cpp
+++ CPP04/ex00/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: mgulenay <mgulenay@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:15:22 by mgulenay          #+#    #+#             */
-/*   Updated: 2022/11/08 16:25:15 by mgulenay         ###   ########.fr       */
+/*   Updated: 2022/11/08 21:19:14 by mgulenay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,29 @@
 
 Dog::Dog(/* args */)
 {
-	
+	std::cout << GREEN << "Default Dog constructoR called " << RESET << std::endl;
 }
 
+Dog::Dog(const Dog &source_obj)
+{
+	std::cout << YELLOW << "Dog Copy ConstructoR called" << RESET << std::endl;
+
+	*this = source_obj;
+}
+
+Dog &Dog::operator=(const Dog &source_obj)
+{
+	std::cout << BLUE << "Dog Assignment OperatoR called" << RESET << std::endl;
+
+	if (this != &source_obj)
+	{
+		this->_type = source_obj._type;
+	}
+	return (*this);
+}
 
 
 Dog::~Dog()
 {
-	
+	std::cout << GREEN << "Dog destructoR called " << RESET << std::endl;
 }
